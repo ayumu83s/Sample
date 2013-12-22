827f4626bcdf92c7e3cac5cc6acde0f02827c9ac
ayumu83s <ayumu.itou.1125@gmail>
2013-12-22 23:16:58 +0900

    ボタンイベント追加

--- src/jp/co/itochin/sample/MainActivity.java
+++ src/jp/co/itochin/sample/MainActivity.java
@@ -3,15 +3,21 @@ package jp.co.itochin.sample;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
 import android.util.Log;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		Log.d("OnCreate", "kita!");
+		Button resetBtn = (Button)findViewById(R.id.reset);
+		resetBtn.setOnClickListener(this);
 	}
 
 	@Override
@@ -20,5 +26,14 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+
+	@Override
+	public void onClick(View v) {
+		Log.d("onClick", "kita!");
+		// TODO 自動生成されたメソッド・スタブ
+		Toast.makeText(this, "reset!", Toast.LENGTH_SHORT).show();
+		
+	}
+	
 
 }
