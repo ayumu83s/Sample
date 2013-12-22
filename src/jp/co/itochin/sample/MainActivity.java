dcf30132cfdd859dbd29f92c1a7b7757e677b67f
ayumu83s <ayumu.itou.1125@gmail>
2013-12-22 22:52:28 +0900

    Logを出力してみる

--- src/jp/co/itochin/sample/MainActivity.java
+++ src/jp/co/itochin/sample/MainActivity.java
@@ -3,6 +3,7 @@ package jp.co.itochin.sample;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.util.Log;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +11,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Log.d("OnCreate", "kita!");
 	}
 
 	@Override
